8f72f0cf81b41b8d3c793c57edb21daa1c6d7d2b
Vinicius Ribeiro Aparecido <vinicius.aparecido@zup.com.br>
2020-05-06 09:55:24 -0300

    changed jobs

--- jenkins_config/jobs.groovy
+++ jenkins_config/jobs.groovy
@@ -3,25 +3,7 @@ def providerUrl = 'https://github.com/vinirib/pact-provider-sample'
 def consumerAppName = 'client-api'
 def providerAppName = 'account-api'
 
-pipelineJob("1-PACT-FLOW-CONSUMER-GENERATE-AND-PUBLISH-CONTRACT") {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url(consumerUrl)
-                    }
-                    branch('master')
-                    extensions {}
-                }
-            }
-             scriptPath("Jenkinsfile-generate-and-publish-contract")
-        }
-    }
-}
-
-
-pipelineJob("2-PACT-FLOW-PROVIDER-TEST-CONTRACT-client-api") {
+pipelineJob("1-PACT-FLOW-PROVIDER-TEST-CONTRACT-client-api") {
     definition {
         cpsScm {
             scm {
@@ -38,30 +20,13 @@ pipelineJob("2-PACT-FLOW-PROVIDER-TEST-CONTRACT-client-api") {
     }
 }
 
-pipelineJob("3-PACT-FLOW-CONSUMER-CAN-I-DEPLOY") {
+pipelineJob("2-PACT-FLOW-CONSUMER-CAN-I-DEPLOY") {
     definition {
         cpsScm {
             scm {
                 git {
                     remote {
                         url(consumerUrl)
-                    }
-                    branch('master')
-                    extensions {}
-                }
-            }
-             scriptPath("Jenkinsfile-can-i-deploy")
-        }
-    }
-}
-
-pipelineJob("PROVIDER-CHANGED-CONTRACT-CHECK-INTEGRATION") {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url(providerUrl)
                     }
                     branch('master')
                     extensions {}
