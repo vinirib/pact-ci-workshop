613be9ad76ece28371e4605f8c483cabf71f95e9
Vinicius Ribeiro Aparecido <vinicius.aparecido@zup.com.br>
2020-05-06 09:58:53 -0300

    organized branches on jobs

--- jenkins_config/jobs.groovy
+++ jenkins_config/jobs.groovy
@@ -3,7 +3,25 @@ def providerUrl = 'https://github.com/vinirib/pact-provider-sample'
 def consumerAppName = 'client-api'
 def providerAppName = 'account-api'
 
-pipelineJob("1-PACT-FLOW-PROVIDER-TEST-CONTRACT-client-api") {
+pipelineJob("1-PACT-FLOW-CONSUMER-GENERATE-AND-PUBLISH-CONTRACT") {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url(consumerUrl)
+                    }
+                    branch('feature/improvements')
+                    extensions {}
+                }
+            }
+             scriptPath("Jenkinsfile-generate-and-publish-contract")
+        }
+    }
+}
+
+
+pipelineJob("2-PACT-FLOW-PROVIDER-TEST-CONTRACT-client-api") {
     definition {
         cpsScm {
             scm {
@@ -11,7 +29,7 @@ pipelineJob("1-PACT-FLOW-PROVIDER-TEST-CONTRACT-client-api") {
                     remote {
                         url(providerUrl)
                     }
-                    branch('feature/endpoint-improvements')
+                    branch('feature/balance-improvements')
                     extensions {}
                 }
             }
@@ -20,13 +38,30 @@ pipelineJob("1-PACT-FLOW-PROVIDER-TEST-CONTRACT-client-api") {
     }
 }
 
-pipelineJob("2-PACT-FLOW-CONSUMER-CAN-I-DEPLOY") {
+pipelineJob("3-PACT-FLOW-CONSUMER-CAN-I-DEPLOY") {
     definition {
         cpsScm {
             scm {
                 git {
                     remote {
                         url(consumerUrl)
+                    }
+                    branch('master')
+                    extensions {}
+                }
+            }
+             scriptPath("Jenkinsfile-can-i-deploy")
+        }
+    }
+}
+
+pipelineJob("PROVIDER-CHANGED-CONTRACT-CHECK-INTEGRATION") {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url(providerUrl)
                     }
                     branch('master')
                     extensions {}
